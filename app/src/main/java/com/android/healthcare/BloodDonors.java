f5072a5f1208a75a6679ba9340cdaab5417f66f9
animesh288 <animesh.satish@gmail.com>
2021-12-23 19:05:51 +0530

    added BloodDonors and EmergencyContacts activities

---
+++ app/src/main/java/com/android/healthcare/BloodDonors.java
@@ -0,0 +1,21 @@
+package com.android.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.android.healthcare.Adapter.Adapter;
+
+import java.util.ArrayList;
+
+public class BloodDonors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blooddonors_screen);
+
+    }
+}
---
+++ app/src/main/java/com/android/healthcare/EmergencyContact.java
@@ -0,0 +1,22 @@
+package com.android.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.android.healthcare.Adapter.Adapter;
+
+import java.util.ArrayList;
+
+public class EmergencyContact extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.emergencycontact_screen);
+
+    }
+}
