53e2dc258b15c1dc5a1a98aecbe9bd731bdfd0cc
animesh288 <animesh.satish@gmail.com>
2021-12-25 16:02:11 +0530

    added hospital details screen

--- app/src/main/java/com/android/healthcare/MainActivity.java
+++ app/src/main/java/com/android/healthcare/MainActivity.java
@@ -5,43 +5,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.healthcare.Adapter.Adapter;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Integer> test;
-    private RecyclerView recyclerView,recyclerView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
-        recyclerView=findViewById(R.id.menu_doc_rv);
-        recyclerView2=findViewById(R.id.menu_hospital_rv);
-        test=new ArrayList<>(14);
-        test.add(0);
-        test.add(0);
-        test.add(0);
-        test.add(0);
-        test.add(0);
-        test.add(0);
-        test.add(0);
-        test.add(0);
-        test.add(0);
-        test.add(0);
 
 
-        Adapter adapter=new Adapter(test);
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,true);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-        Adapter adapter1=new Adapter(test);
-        RecyclerView.LayoutManager layoutManager1=new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,true);
-        recyclerView2.setLayoutManager(layoutManager1);
-        recyclerView2.setAdapter(adapter1);
 
 
     }
