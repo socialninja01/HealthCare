1585ae9473dd940a9fa7060ab49c287130fb8f5f
animesh288 <animesh.satish@gmail.com>
2022-01-04 16:05:33 +0530

    added terms_and_conditions screen

--- app/src/main/java/com/android/healthcare/MainActivity.java
+++ app/src/main/java/com/android/healthcare/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_screen);
+        setContentView(R.layout.terms_conditions_screen);
 
     }
 }
