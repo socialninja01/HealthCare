8ae990a70cb7bfa41bfc5560f5077cb7b658292e
animesh288 <animesh.satish@gmail.com>
2022-01-06 01:40:51 +0530

    added menu

--- app/src/main/java/com/android/healthcare/MainActivity.java
+++ app/src/main/java/com/android/healthcare/MainActivity.java
@@ -1,24 +1,31 @@
 package com.android.healthcare;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toolbar;
 
 import com.android.healthcare.Adapter.Adapter;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.doctor_profile_screen);
-
+        setContentView(R.layout.home_screen);
     }
+
 }
