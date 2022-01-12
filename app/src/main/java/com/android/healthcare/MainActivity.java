ef57b3c2bb9742015740735555632c92d737770b
animesh288 <animesh.satish@gmail.com>
2022-01-13 01:41:11 +0530

    changed to constraint layout

--- app/src/main/java/com/android/healthcare/MainActivity.java
+++ app/src/main/java/com/android/healthcare/MainActivity.java
@@ -19,13 +19,13 @@ import com.google.android.material.navigation.NavigationView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.splash_screen);
     }
 
 }
