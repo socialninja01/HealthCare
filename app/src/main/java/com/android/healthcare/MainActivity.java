504a38b11e3a76a0ed540379ac8270952d26fbab
animesh288 <animesh.satish@gmail.com>
2022-01-15 16:38:32 +0530

    updated layouts

--- app/src/main/java/com/android/healthcare/MainActivity.java
+++ app/src/main/java/com/android/healthcare/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_screen);
+        setContentView(R.layout.about_screen);
     }
 
 }
